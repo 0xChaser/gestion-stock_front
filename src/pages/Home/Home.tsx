95e40cf2b3b4b2cf867ec51107cb5ed669d015da
Chaser <florian.isak@icloud.com>
2024-06-15 21:20:02 +0200

    Fix HomePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { CssBaseline, ThemeProvider, createTheme, Button, Box, Typography } from '@mui/material';
 import StockImage from '../../assets/images/image_stock.png';
 import { useTheme } from '../../contexts/themeContext';
+import CustomTitle from 'components/titles/CustomTitle';
 
 const HomePage: React.FC = () => {
   const { darkMode } = useTheme();
@@ -20,10 +21,10 @@ const HomePage: React.FC = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box sx={{ textAlign: 'center', padding: 3 }}>
-        <Typography variant="h3" style={{ color: darkMode ? '#96CD32' : '#232876', marginBottom: 20 }}>
+        <CustomTitle>
           Bienvenue sur E-Stock!
-        </Typography>
-        <img src={StockImage} alt="Stock" style={{ maxWidth: '40%', height: 'auto' }} />
+        </CustomTitle>
+        <img src={StockImage} alt="Stock" style={{ minWidth: '45%', width:'100%', maxWidth:'1000px', height: 'auto' }} />
         <Box sx={{ marginTop: 3 }}>
           <Button component={Link} to="/stock" variant="contained" color="primary">
             Voir le stock
